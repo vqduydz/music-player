a82ec592b04e3355c9355ad6300a054e5cab0b09
vqduydz <vqduydz@gmail.com>
2022-07-28 17:23:21 +0700

    fs comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '7d2c259670msh71fa54b1c985b5dp1f4736jsnf0845a23cdd6',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
+        },
+    };
+
+    fetch('https://spotify23.p.rapidapi.com/search/?q=love&type=multi&offset=0&limit=10&numberOfTopResults=5', options)
+        .then((response) => response.json())
+        .then((response) => console.log(response))
+        .catch((err) => console.error(err));
+
+    return <div className="App"></div>;
 }
 
 export default App;
